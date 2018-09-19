1056d5eac09cc596f09e5856b6d6aaf4fe8f665d
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-09-19 16:57:13 +0800

    New year chaos

---
+++ Arrays/NewYearChaos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class NewYearChaos
+    {
+        public static void minimumBribes(int[] q)
+        {
+            int temp = 0;
+            int temp2 = 0;
+            int ctr = 0;
+            bool chaotic = false;
+            foreach (int i in q)
+            {
+                bool matched = true;
+                for (int x = 0; x < q.Length - 1; x++)
+                {
+                    if (q[x] - (x + 1) > 2)
+                    {
+                        chaotic = true;
+                        break;
+                    }
+                    if (q[x] > q[x + 1])
+                    {
+                        temp = q[x + 1];
+                        temp2 = q[x];
+                        q.SetValue(temp2, x + 1);
+                        q.SetValue(temp, x);
+                        ctr++;
+                    }
+
+                    if (q[x] != x + 1)
+                    {
+                        matched = false;
+                    }
+                }
+                if (matched) { break; }
+            }
+            if (chaotic)
+            {
+                Console.WriteLine("Too chaotic");
+            }
+            else
+            {
+                Console.WriteLine(ctr);
+            }
+        }
+    }
+}
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,11 +10,13 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            var a = LeftRotate.RotLeft(new int[] { 1, 2, 3, 4, 5 }, 4);
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.WriteLine(a[i]);
-            }
+            //var a = LeftRotate.RotLeft(new int[] { 1, 2, 3, 4, 5 }, 4);
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+
+            NewYearChaos.minimumBribes(new int[] { 2, 1, 5, 3, 4 });
 
             Console.Read();
         }
