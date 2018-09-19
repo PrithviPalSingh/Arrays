a57a15d17db515db53c63a15951e20dfc7d385de
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-09-19 12:23:36 +0800

    Add project files.

---
+++ Arrays/LeftRotate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class LeftRotate
+    {
+        public static int[] RotLeft(int[] a, int d)
+        {
+            var finalArray = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i >= d)
+                {
+                    var t = i - d;
+                    finalArray[t] = a[i];
+                }
+                else
+                {
+                    var t = a.Length - d + i;
+                    finalArray[t] = a[i];
+                }
+            }
+
+            return finalArray;
+        }
+
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = LeftRotate.RotLeft(new int[] { 1, 2, 3, 4, 5 }, 4);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+            Console.Read();
+        }
+    }
+}
